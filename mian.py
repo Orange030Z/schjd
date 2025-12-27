5e297f52193aec136ae0fe53d21cda8b4f64d393
Orange030Z <131943112+Orange030Z@users.noreply.github.com>
2025-12-27 20:45:38 +0800

    更新 mian.py

--- mian.py
+++ mian.py
@@ -18,17 +18,63 @@ def get_region_free(addr):
     2. 尝试反向 DNS 解析识别
     3. 兜底使用一个极简公共接口
     """
-    # 常见机场/服务器节点特征库
+       # 终极扩充版特征库：覆盖全球主流及小众国家、核心城市、机场代码
     features = {
-        'hk|hkg|hongkong': '香港',
-        'tw|taiwan|tpe': '台湾',
-        'jp|japan|tokyo|nrt|hnd': '日本',
-        'sg|singapore|sin': '新加坡',
-        'us|america|unitedstates|lax|sfo|iad': '美国',
-        'kr|korea|icn|seoul': '韩国',
-        'de|germany|fra': '德国',
-        'uk|london|gb': '英国'
+        # --- 亚洲 & 太平洋 ---
+        'hk|hkg|hongkong|香港|pccw|hkt': '香港',
+        'tw|taiwan|tpe|hinet|cht|台湾|台北': '台湾',
+        'jp|japan|tokyo|nrt|hnd|kix|osaka|日本|东京|大阪|埼玉': '日本',
+        'sg|singapore|sin|新加坡': '新加坡',
+        'kr|korea|icn|seoul|sel|韩国|首尔|春川': '韩国',
+        'th|thailand|bkk|bangkok|泰国|曼谷': '泰国',
+        'vn|vietnam|hanoi|sgn|越南|河内|胡志明': '越南',
+        'my|malaysia|kul|吉隆坡|马来西亚': '马来西亚',
+        'ph|philippines|mnl|manila|菲律宾|马尼拉': '菲律宾',
+        'id|indonesia|cgk|jakarta|印尼|雅加达': '印尼',
+        'in|india|bom|del|mumbai|印度|孟买': '印度',
+        'ae|uae|dubai|dxb|迪拜|阿联酋': '阿联酋',
+        'au|australia|syd|mel|bne|澳大利亚|悉尼|墨尔本|布里斯班': '澳大利亚',
+        'nz|newzealand|akl|新西兰|奥克兰': '新西兰',
+        'kh|cambodia|phnompenh|柬埔寨|金边': '柬埔寨',
+        'pk|pakistan|khi|巴基斯坦': '巴基斯坦',
+        'kz|kazakhstan|ala|哈萨克斯坦': '哈萨克斯坦',
+        
+        # --- 北美 & 南美 ---
+        'us|america|unitedstates|usa|lax|sfo|iad|ord|sea|美国|洛杉矶|圣何塞|西雅图|纽约|芝加哥': '美国',
+        'ca|canada|yvr|yyz|mtl|加拿大|温哥华|多伦多|蒙特利尔': '加拿大',
+        'br|brazil|sao|brazil|巴西|圣保罗': '巴西',
+        'mx|mexico|mex|墨西哥': '墨西哥',
+        'ar|argentina|bue|阿根廷': '阿根廷',
+        'cl|chile|scl|智利': '智利',
+        'co|colombia|bog|哥伦比亚': '哥伦比亚',
+        
+        # --- 欧洲 ---
+        'de|germany|fra|frankfurt|德国|法兰克福': '德国',
+        'uk|gb|london|lon|lhr|英国|伦敦': '英国',
+        'fr|france|par|paris|法国|巴黎': '法国',
+        'nl|netherlands|ams|amsterdam|荷兰|阿姆斯特丹': '荷兰',
+        'ru|russia|moscow|mow|svo|俄罗斯|莫斯科|新西伯利亚': '俄罗斯',
+        'tr|turkey|ist|istanbul|土耳其|伊斯坦布尔': '土耳其',
+        'it|italy|mil|milano|意大利|米兰': '意大利',
+        'es|spain|mad|madrid|西班牙|马德里': '西班牙',
+        'ch|switzerland|zrh|zurich|瑞士|苏黎世': '瑞士',
+        'se|sweden|sto|stockholm|瑞典|斯德哥尔摩': '瑞典',
+        'at|austria|vie|vienna|奥地利|维也纳': '奥地利',
+        'pl|poland|waw|warsaw|波兰|华沙': '波兰',
+        'no|norway|osl|oslo|挪威|奥斯陆': '挪威',
+        'fi|finland|hel|helsinki|芬兰|赫尔辛基': '芬兰',
+        'ie|ireland|dub|dublin|爱尔兰|都柏林': '爱尔兰',
+        'ua|ukraine|kiev|iev|乌克兰|基辅': '乌克兰',
+        'gr|greece|ath|athens|希腊|雅典': '希腊',
+        'pt|portugal|lis|lisbon|葡萄牙|里斯本': '葡萄牙',
+        
+        # --- 非洲 ---
+        'za|southafrica|jnb|johannesburg|南非': '南非',
+        'eg|egypt|cai|cairo|埃及|开罗': '埃及',
+        'ng|nigeria|los|尼日利亚': '尼日利亚',
+        'ma|morocco|cas|摩洛哥': '摩洛哥'
     }
+
 
     # 1. 直接检查地址字符串（如果是域名）
     addr_lower = addr.lower()
